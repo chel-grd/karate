2b73c74a47e51fa6521f7a55af152dd3e9470a1e
chel-grd <chel.grd@gmail.com>
2017-03-18 17:03:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateScreen() {	
 $(".foot01").html("<p style='padding-left:0px;'>&copy; " + new Date().getFullYear() + " Saskatoon Shotokan Karate. All rights reserved.</p>");
 document.getElementById("nav02").innerHTML = 
-"<div>" +
+"<div id='navMenu'>" +
 "<ul class='nav'>" +
 "<li id='indexTab'><button id='homeButton'>Home</button></li>" +
 "<li id='aboutTab'><button id='aboutButton'>About</button>" +
